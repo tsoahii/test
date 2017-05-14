283a49e802a926e947faa9c4b0d0e1193a58fba7
tsoahii <marcelwenting@gmail.com>
2017-05-14 10:05:01 +0200

    removed compiling errors and formatted file

--- main.cpp
+++ main.cpp
@@ -1,20 +1,18 @@
 // explanation of the program
 
-#include <math>
-#include <vectors>
+#include <iostream>
+#include <string>
+#include <vector>
 
-using namespace std;
 namespace {
-class myClass {
-  public:
-    int add(int a, int b)
-}
+int add(int a, int b);
 }
 
 int main() {
   int a = 5;
   int b = 6;
   int loop_count = 0;
+  bool ifeellikeit = true;
 
   if (ifeellikeit) {
     loop_count = 10;
@@ -26,16 +24,26 @@ int main() {
   }
 
   for (int i = 0; i < loop_count; ++i) {
-    cout << "result " << i << " -->" << add(a, b) << "\n";
+    std::cout << "result " << i << " --> " << add(a, b) << "\n";
     ++a;
     ++b;
   }
 
   // ugly formatted if
   int shit = 0;
-  if(shit){
-  cout << "bah humbug!" << "\n";
-} else{}
+  if (shit) {
+    std::cout << "bah humbug!" << "\n";
+  }
+  else {
+  }
 
   return 0;
 }
+
+namespace {
+int add(int a, int b) {
+  int result = 0;
+  result = a + b;
+  return result;
+}
+} // namespace
